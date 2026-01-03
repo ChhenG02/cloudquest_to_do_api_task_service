b17c50161071aceba3a758bddc7c04b00f94b0ff
iuMIK <macbookair@iuMIK.local>
2026-01-03 17:34:17 +0700

    add index file

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { webcrypto } from 'crypto';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
   if (!globalThis.crypto) {
     (globalThis as any).crypto = webcrypto;
   }
-  const app = await NestFactory.create(AppModule);
+  
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
   await app.listen(process.env.PORT ?? 3003);
 }
 bootstrap();
